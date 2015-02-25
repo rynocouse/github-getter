b5c03a85f01af7630b0f53313a6f9c4c507c3fc8
J Scott Smith <jscsmith@gmail.com>
2015-02-25 13:57:29 -0800

    update gulp file with scss tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,36 @@
 var jshint = require('gulp-jshint'),
     stylish = require('jshint-stylish'),
     watch = require('gulp-watch'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
     gulp = require('gulp');
+
+// Watch SCSS then Compile
+
+gulp.task('watch', function () {
+  gulp.watch('js/app.js', ['lint']);
+  gulp.watch('scss/*.scss', ['scss']);
+});
+
+// Sass Compile
+// And Auto Prefixer
+
+gulp.task('scss', function () {
+  gulp.src('scss/*.scss')
+    .pipe(sass())
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('css'))
+});
 
 // JS Lint task
 
 gulp.task('lint', function() {
-  return gulp.src('./lib/*.js')
-  .pipe(jshint())
-  .pipe(jshint.reporter(stylish));
-});
-
-// Watch JS then Lint
-
-gulp.task('watch', function () {
-  gulp.src('js/*.js')
-  .pipe(watch('js/*.js'))
+  return gulp.src('js/app.js')
   .pipe(jshint())
   .pipe(jshint.reporter(stylish));
 });
 
 
 gulp.task('default', function() {
-  // place code for your default task here
+  gulp.start('watch');
 });
