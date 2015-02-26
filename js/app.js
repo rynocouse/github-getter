514d043e09260b1c978fc59aca07543e23f7dcea
Ryan Couse <ryan@bigstormstudio.com>
2015-02-26 12:03:17 -0800

    use object literal pattern

--- js/app.js
+++ js/app.js
@@ -1,185 +1,172 @@
-(function(window, $, _) {
+var githubGetter = (function(window, $, _) {
 
-  // Setup cache object to store searches
-  var cache = {},
-      repoData;
-
-  // Cache $ selectors
-  var $body = $('body'),
-      $loader = $('#loader'),
-      $search = $('#search'),
-      $overlayContainer = $('#overlay-container'),
-      $resultsContainer = $('#results-container');
-
-
-  // Events
-
-  // Bind search function to input
-  $search.on('keypress', function(e) {
-
-    if (e.which == 13) {
-      searchQuery();
+    var helpers = {
+        slugify: function(text) {
+            return text.toString().toLowerCase()
+                .replace(/\s+/g, '-') // Replace spaces with -
+                .replace(/[^\w\-]+/g, '') // Remove all non-word chars
+                .replace(/\-\-+/g, '-') // Replace multiple - with single -
+                .replace(/^-+/, '') // Trim - from start of text
+                .replace(/-+$/, ''); // Trim - from end of text
+        }
     }
 
-  });
 
-  // Close Overlay
-  $overlayContainer.on('click', function() {
+    return {
 
-    $overlayContainer.removeClass('active');
+        initialize: function() {
+            this.cache = {};
+            this.bindUIElements();
+            this.bindEvents();
+        },
 
-    // Unlock scrolling on the body
-    $body.removeClass('locked');
+        bindUIElements: function() {
+            this.ui = this.ui || {};
+            _.extend(this.ui, {
+                body: $('body'),
+                loader: $('#loader'),
+                search: $('#search'),
+                overlayContainer: $('#overlay-container'),
+                resultsContainer: $('#results-container')
+            });
+            console.log(this.ui);
+        },
 
-  });
-
-
-  // Get the search query from the input
-  function searchQuery() {
-
-    var query = $search.val();
-
-    window.query = query;
-
-    searchGithub(query);
-
-  }
-
-  // Search through github repos
-  function searchGithub(query) {
-
-    // Show Loader
-    $loader.addClass('active');
-    // Hide previous results
-    $resultsContainer.fadeOut(400);
+        bindEvents: function() {
+            // Bind search function to input
+            this.ui.search.on('keypress', _.bind(this.onSearchQuery, this));
+            // Close Overlay
+            this.ui.overlayContainer.on('click', _.bind(this.onClickOverlay, this));
+        },
 
 
-    // Check if the query has already been made
-    console.log(getCache(query));
-    if (getCache(query)) {
-      renderResults(query, getCache(query));
-    } else {
-      ajaxRequest(query);
-    }
-  }
 
-  function slugify(text) {
-    return text.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, ''); // Trim - from end of text
-  }
+        onSearchQuery: function(e) {
+            if (e.which == 13) {
+                this.searchGithub(this.ui.search.val());
+            }
+        },
 
-  // Look for the query in cache
+        onClickOverlay: function(e) {
+            this.ui.overlayContainer.removeClass('active');
+            // Unlock scrolling on the body
+            this.ui.body.removeClass('locked');
+        },
 
-  function getCache(query) {
-    return cache[slugify(query)] || false;
-  }
+        searchGithub: function(query) {
 
-  function renderResults(query, data) {
-    console.log(query, data);
-    $loader.removeClass('active');
+            this.query = query;
+            // Show Loader
+            this.ui.loader.addClass('active');
+            // Hide previous results
+            this.ui.resultsContainer.fadeOut(400);
 
-    // add the query to the response object
-    data.query = query;
+            // Check if the query has already been made
+            console.log(this.getCache());
+            if (this.getCache()) {
+                this.renderResults(this.getCache().results);
+            } else {
+                this.ajaxRequest();
+            }
+        },
 
-    // render the template with
-    // the search results
-    resultsTemplate(data);
-    repoInfo(data);
+        getCache: function() {
+            // Look for the query in cache
+            return this.cache[helpers.slugify(this.query)] || false;
+        },
 
-    // add response to cache object
-    cache[slugify(query)] = data;
+        renderResults: function(data) {
+            console.log(this.query, data);
+            this.ui.loader.removeClass('active');
 
-    window.repoData = data;
+            // add the query to the response object
+            data.query = this.query;
 
-    console.log(window.data);
-  }
+            var searchResults = {
+                query: this.query,
+                results: data
+            }
+            this.repoData = searchResults;
+
+            // render the template with
+            // the search results
+            this.renderRepoList(searchResults);
+            this.renderRepoInfo(searchResults);
+
+            // add response to cache object
+            this.cache[helpers.slugify(this.query)] = searchResults;
+        },
+
+        ajaxRequest: function() {
+            console.log('ajaxRequest', this.query);
+
+            // Set the url with the search query
+            var urlQuery = 'https://api.github.com/legacy/repos/search/' + this.query;
+
+            // Make an ajax call to the github api
+            $.getJSON(urlQuery, {
+                    format: "json",
+                })
+                .done(_.bind(this.renderResults, this))
+                .fail(_.bind(this.handleError, this));
+        },
+
+        handleError: function() {
+            this.ui.loader.removeClass('active');
+        },
+
+        renderRepoList: function(data) {
+            // Render the underscore template and
+            // inject in our search results.
+            this.ui.resultsContainer.html(this.resultsTemplate(data)).hide().fadeIn(500);
+        },
+
+        // Getting More Info on a Repo
+        renderRepoInfo: function(data) {
+
+            // Click event for displaying the repo info
+            this.ui.resultsContainer.on('click', '#repo-info', _.bind(function(e) {
+
+                var i = $(e.currentTarget).data('index');
+                var currentRepo = this.repoData.results.repositories[i];
+
+                // Render the underscore template and inject in our search results.
+                this.ui.overlayContainer.html(this.repoinfoTemplate(currentRepo)).addClass('active');
+                // Lock scrolling on the body
+                this.ui.body.addClass('locked');
+
+            }, this));
+        },
 
 
-  // The ajax request
+        resultsTemplate: _.template(
+            '<h2>Search Results for <em><%- query %></em></h2>' +
+            '<div class="repo-list">' +
+            '<div class="repo-head"><span class="repo-name">Name</span><span class="repo-owner">Owner</span></div>' +
+            '<% _.each( results.repositories, function( repository, i ){ %>' +
+            '<a id="repo-info" data-index="<%- i %>">' +
+            '<div class="repo">' +
+            '<span class="repo-name"><%- repository.name %></span>' +
+            '<span class="repo-owner"><%- repository.owner %></span>' +
+            '</div>' +
+            '</a>' +
+            '<% }); %>' +
+            '<div>'
+        ),
 
-  function ajaxRequest(query) {
-    console.log('ajaxRequest', query);
+        repoinfoTemplate: _.template(
+            '<article class="repo-details wrapper">' +
+            '<h2>Repository Details for <em><%- name %></em></h2>' +
+            '<p>Language: <strong><%- language %></strong></p>' +
+            '<p>Owner: <strong><%- owner %></strong></p>' +
+            '<p>Followers: <strong><%- followers %></strong></p>' +
+            '<p>Description: <%- description %></p>' +
+            '<a href="<%- url %>" class="button">GitHub Link</a>' +
+            '</article>'
+        )
 
-    // Set the url with the search query
-    var urlQuery = 'https://api.github.com/legacy/repos/search/' + query;
-
-    // Make an ajax call to the github api
-    $.getJSON(urlQuery, {
-      format: "json",
-    }).done(function(data) {
-      renderResults(query, data);
-    }).fail(function() {
-      // Hide Loader
-      $loader.removeClass('active');
-    });
-  }
-
-
-  function resultsTemplate(data) {
-
-    // pre-compile the template
-
-    var template = _.template(
-      '<h2>Search Results for <em>' + window.query + '</em></h2>' +
-      '<div class="repo-list">' +
-      '<div class="repo-head"><span class="repo-name">Name</span><span class="repo-owner">Owner</span></div>' +
-      '<% _.each( repositories, function( repositories, i ){ %>' +
-        '<a id="repo-info" data-index="<%- i %>">' +
-          '<div class="repo">' +
-            '<span class="repo-name"><%- repositories.name %></span>' +
-            '<span class="repo-owner"><%- repositories.owner %></span>' +
-          '</div>' +
-        '</a>' +
-      '<% }); %>' +
-      '<div>'
-    );
-
-    // Render the underscore template and
-    // inject in our search results.
-    $resultsContainer.html(template(data)).hide().fadeIn(500);
-  }
-
-
-  // Getting More Info on a Repo
-  function repoInfo(data, repoData) {
-
-    // Click event for displaying the repo info
-    $resultsContainer.on('click', '#repo-info', function(e, repoData) {
-
-      var i = $(e.currentTarget).data('index');
-      var currentRepo = window.repoData.repositories[i];
-
-      displayRepoInfo(currentRepo);
-
-    });
-
-  }
-
-  // Display a modal and render
-  // the repo details to template
-
-  function displayRepoInfo(currentRepo) {
-
-    // pre-compile the template
-    var template = _.template(
-      '<article class="repo-details wrapper">' +
-        '<h2>Repository Details for <em><%- name %></em></h2>' +
-        '<p>Language: <strong><%- language %></strong></p>' +
-        '<p>Owner: <strong><%- owner %></strong></p>' +
-        '<p>Followers: <strong><%- followers %></strong></p>' +
-        '<p>Description: <%- description %></p>' +
-        '<a href="<%- url %>" class="button">GitHub Link</a>' +
-      '</article>'
-    );
-
-    // Render the underscore template and inject in our search results.
-    $overlayContainer.html(template(currentRepo)).addClass('active');
-    // Lock scrolling on the body
-    $body.addClass('locked');
-  }
+    };
 
 })(window, jQuery, _);
+
+githubGetter.initialize(); //Boom
